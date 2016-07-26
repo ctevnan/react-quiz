15c9eb1beb7d42bee418063102e99fd93abe5460
ctevnan <carolyntevnan@yahoo.com>
2016-07-26 17:32:24 -0400

    setting up app.jsx for 4 questions total

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,86 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      questions: [
+        {
+          id: 1,
+          text: 'What is your name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Thor'
+            },
+            { id: 'b',
+              text: 'George'
+            },
+            {
+              id: 'c',
+              text: 'Chris'
+            }    
+          ],
+          correct: 'b'
+        },
+        {
+          id: 2,
+          text: 'What is your name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Thor'
+            },
+            { id: 'b',
+              text: 'George'
+            },
+            {
+              id: 'c',
+              text: 'Chris'
+            }    
+          ],
+          correct: 'b'
+        },
+        {
+          id: 3,
+          text: 'What is your name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Thor'
+            },
+            { id: 'b',
+              text: 'George'
+            },
+            {
+              id: 'c',
+              text: 'Chris'
+            }    
+          ],
+          correct: 'b'
+        },
+        {
+          id: 4,
+          text: 'What is your name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Thor'
+            },
+            { id: 'b',
+              text: 'George'
+            },
+            {
+              id: 'c',
+              text: 'Chris'
+            }    
+          ],
+          correct: 'b'
+        },
+      ]    
+    }
+  }
+
   render(){
     return(
       <div>
