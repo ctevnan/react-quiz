89b95df264561b73c1db9c51de146f103b8fb69a
ctevnan <carolyntevnan@yahoo.com>
2016-07-28 16:21:51 -0400

    index.js still not working

--- componentsF/App.jsx
+++ componentsF/App.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import QuestionList from './quiz/QuestionList.jsx'
+import QuestionList from './quizF/QuestionList.jsx';
 
 class App extends Component{
   constructor(props){
